d0193a1537cb5922b00d7bd93bdb249125117785
Soraya <icuestiona@gmail.com>
2024-01-14 17:02:32 +0100

    Suffix added and name instead of id in the function

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const inputs = document.querySelectorAll(".controls input");
 
+function updateVariables() {
+  const suffix = this.dataset.sizing || "";
+
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
+
 inputs.forEach((input) => {
   input.addEventListener("input", updateVariables);
 });
-
-function updateVariables() {
-  document.documentElement.style.setProperty(
-    `--${this.id}`,
-    this.value + (this.dataset.sizing || "")
-  );
-}
